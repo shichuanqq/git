8d8328f71eff26e2901c648443263f43423731f1
shichuan <shichuanqaq@163.com>
2019-11-30 22:50:44 +0800

    提交测试代码

---
+++ src/main/java/com/example/demo/controller/createDetailController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class createDetailController {
+
+    @GetMapping("/test")
+    public void test(){
+        System.out.println(111);
+    }
+}
