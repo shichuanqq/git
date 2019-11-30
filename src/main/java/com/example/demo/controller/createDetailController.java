9f60abddca130045313b7d359c4696354a067bed
shichuan <shichuanqaq@163.com>
2019-12-01 00:01:08 +0800

    整合redis

--- src/main/java/com/example/demo/controller/createDetailController.java
+++ src/main/java/com/example/demo/controller/createDetailController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.utils.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class createDetailController {
 
+    @Autowired
+    RedisUtils redisUtils;
+
     @GetMapping("/test")
     public void test(){
-        System.out.println(111);
-        System.out.println(111);
-        System.out.println(111);
+        redisUtils.set("a","test");
+        System.out.println(redisUtils.get("a"));
     }
 }
