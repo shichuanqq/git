e545f07341693b37abceeaaddad1e66482a32e7e
shichuan <shichuanqaq@163.com>
2019-12-07 13:24:13 +0800

    整合数据库，以及切换数据源读数据

--- src/main/java/com/example/demo/config/database/MybatisConfig.java
+++ src/main/java/com/example/demo/config/database/MybatisConfig.java
@@ -4,7 +4,6 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
